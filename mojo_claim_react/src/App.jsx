5318c925140a414656c1f5c1a786e5c435994ac7
Fletcher Christian <fletcher@jamkillerproductions.io>
2025-03-07 21:33:10 -0600

    Commit

--- mojo_claim_react/src/App.jsx
+++ mojo_claim_react/src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
       <div className="content">
         <img src="https://bafybeig6dpytw3q4v7vzdy6sb7q4x3apqgrvfi3zsbvb3n6wvs5unfr36i.ipfs.dweb.link?filename=480.gif" alt="Mojo Logo" />
         <h1>Mojo Claim</h1>
-        <WalletConnect />
+
       </div>
     </div>
   );
